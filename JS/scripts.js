363b2bfd8ba8841e83c92fc604267bb8d4773545
Gonzalo Vera <gonzalo.vera.cerda@gmail.com>
2026-02-19 17:33:21 -0300

    Add files via upload

---
+++ JS/scripts.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#137fec",
+                "background-light": "#f6f7f8",
+                "background-dark": "#101922",
+            },
+            fontFamily: {
+                "display": ["Inter", "sans-serif"]
+            },
+            borderRadius: {
+                "DEFAULT": "0.25rem",
+                "lg": "0.5rem",
+                "xl": "0.75rem",
+                "full": "9999px"
+            },
+        },
+    },
+}
